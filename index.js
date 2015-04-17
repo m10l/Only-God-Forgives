9656046bfd6046197f349a856149028c8b045228
Mike <mike@simpleweb.co.uk>
2015-04-17 13:09:45 +0100

    Added MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('newrelic');
 var twilio     = require('twilio');
-var twiml      = new twilio.TwimlResponse();
 var express    = require('express');
-var app        = express();
+var mongoose   = require('mongoose');
 var xml        = require('xml');
 var bodyParser = require('body-parser');
+
+var app        = express();
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({
     extended: true
@@ -13,7 +14,20 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.set('port', process.env.PORT || 3000);
 
-var recordings = [];
+/**
+ * MongoDB Stuffs
+ */
+
+var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/confessions';
+
+mongoose.connect( mongoUri );
+var Confession = mongoose.model('Confession', {
+    recording_url : String,
+    from          : String,
+    votes         : Number
+});
+
+var twiml = new twilio.TwimlResponse();
 
 app.get('/receivecall', function (req, res) {
     twiml.say('BUTTS!');
@@ -26,24 +40,43 @@ app.post('/record', function (req, res) {
     console.log( req.body.RecordingUrl );
     console.log( req.body.Caller );
 
-    recordings.push({
+    var confession = new Confession ({
         recording_url : req.body.RecordingUrl,
         from          : req.body.from,
         votes         : 0
     });
+    confession.save( function (err) {
+        console.log('Saved confession to DB');
+    });
 
-    console.log(recordings);
-
+    // Send hangup response
     res.set('Content-Type', 'text/xml');
     res.send('<?xml version="1.0" encoding="UTF-8"?><Response><Hangup/></Response>');
 });
 
 app.get('/', function (req, res) {
-    res.render('index', {
-        recordings: recordings
+
+    var confessionsArray = [];
+    Confession.find( function(err, confessions) {
+        if (err) return console.log(err);
+        console.log(confessions);
+
+        confessions.forEach( function(confession) {
+            confessionsArray.unshift({
+                recording_url : confession.recording_url,
+                from          : confession.from,
+                votes         : confession.votes
+            });
+        });
+
+        res.render('index', {
+            recordings: confessionsArray
+        });
+
     });
+
 });
 
 var server = app.listen(app.get('port'), function () {
-    console.log('hiya');
+    console.log( 'App running on port:' + app.get('port') );
 });
